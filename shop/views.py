37838eff21cfdabed89eebf13d5ecb1b4f1d084e
Oamnn <olesgomol@gmail.com>
2025-06-03 13:21:23 +0300

    добавив Promo model, admin customization, product image support
    зробив норм _layout.html, добавив карусельку як на rozetka.com.ua

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from .models import Product  # ОБОВ’ЯЗКОВО
+from .models import Product, Promo  # ОБОВ’ЯЗКОВО
 
 def home(request):
     products = Product.objects.all()
-    return render(request, 'shop/home.html', {'products': products})
+    Promos = Promo.objects.filter(display=True).order_by('-created_at')
+
+    return render(request, 'shop/home.html', {
+        'promos': Promos,
+        'products': products,})
