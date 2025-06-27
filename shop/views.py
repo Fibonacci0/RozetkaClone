4dd90e79ab8511e4f19b736e4ea7a4ef933a745f
Neo-010 <arsen2008melnychuk@gmail.com>
2025-06-27 13:31:09 +0300

    add user

--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,7 @@ from .models import Product  # ОБОВ’ЯЗКОВО
 def home(request):
     products = Product.objects.all()
     return render(request, 'shop/home.html', {'products': products})
+
+def user(request):
+    products = Product.objects.all()
+    return render(request, 'shop/user.html', {'products': products})
