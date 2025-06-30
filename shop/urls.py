08d1e1e1369a106fb3aa82eb8c3d39a72b6df8ab
Neo-010 <arsen2008melnychuk@gmail.com>
2025-07-01 00:21:33 +0300

    basket

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('sign-in/', views.sign_in_view, name='signIn'),
-    
+    path('', views.home, name='home'),    
 ]
