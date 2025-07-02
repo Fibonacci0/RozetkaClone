06c9822bb82cc785a20136651c84805bd8e2020b
Neo-010 <arsen2008melnychuk@gmail.com>
2025-07-02 21:34:18 +0300

    + admain panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,17 +1,19 @@
-
-
 from django.contrib import admin
 from django.utils.html import format_html
 from .models import Product, Promo
 
-# Приклад модельки
-#admin.site.register([])  # Register your models here, e.g., admin.site.register(MyModel)
-
-# кастомні модельки
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'available', 'created_at', 'preview_image')
     fields = ('name', 'description', 'image', 'image_url', 'price', 'available')
+
+    class Media:
+        css = {
+            'all': ('css/custom_admin.css',)  # шлях до вашого CSS у static
+        }
+        js = (
+            # якщо потрібні якісь JS файли, вказуйте тут
+        )
 
     def preview_image(self, obj):
         return format_html('<img src="{}" width="50" height="50" />', obj.get_image())
@@ -20,9 +22,14 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 @admin.register(Promo)
-class ProductAdmin(admin.ModelAdmin):
+class PromoAdmin(admin.ModelAdmin):
     list_display = ('name', 'display', 'created_at', 'preview_image')
     fields = ('name', 'display', 'image', 'image_url', 'created_at')
+
+    class Media:
+        css = {
+            'all': ('css/custom_admin.css',)
+        }
 
     def preview_image(self, obj):
         return format_html('<img src="{}" width="50" height="50" />', obj.get_image())
